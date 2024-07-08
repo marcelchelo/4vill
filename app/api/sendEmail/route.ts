db8b87429ab25e556a36771c12178717fc577739
Marcelo <marcelo95326@icloud.com>
2024-07-08 12:30:43 -0400

    removed form data

--- app/api/sendEmail/route.ts
+++ app/api/sendEmail/route.ts
@@ -2,8 +2,7 @@ import { NextResponse, NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
 
-    //log the request body
-    console.log(formData);  
+
   return NextResponse.json({
     messsage: "email sent successfully",
   });
