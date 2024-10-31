53bf34d5744ca996f8a612cd968dca436e030066
Marcelo Villalba <marcelo95326@icloud.com>
2024-10-31 14:29:03 -0400

    Improved the nav bar

--- app/src/components/NavBarComponent.tsx
+++ app/src/components/NavBarComponent.tsx
@@ -5,19 +5,20 @@ export default function NavBarComponent() {
     <div className='bg-gradient-to-b from-black to-zinc-800 p-4'>
       <nav className='flex items-center justify-between'>
         <div className='text-white font-bold text-xl dark:text-yellow-400'>
-          <h1 className='font-mono font-bold sm:hidden'>4Vill corp</h1>
-          <h1 className='font-mono font-bold hidden sm:block'>4Vill. Contracting</h1>
+          <Link href='/' legacyBehavior>
+              <a className='font-mono font-bold sm:hidden'>4Vill corp</a>
+            </Link>
+            <Link href='/' legacyBehavior>
+              <a className='font-mono font-bold hidden sm:block'>4Vill. Contracting</a>
+            </Link>
         </div>
-        <ul className='flex space-x-4 text-2xl font-thin'>
+
+
+        {/* Other Links */}
+        <ul className='flex space-x-2 text-xl font-normal'>
+          
           <li>
-           
-            <Link className='text-white hover:text-yellow-400' href='/'>Home</Link>
-          </li>
-          <li>
-            <a href='/services' className='text-white  hover:text-yellow-400'>Services</a>
-          </li>
-          <li>
-            <a href='/contact' className='text-white  hover:text-yellow-400'>Contact</a>
+            <a href='/getQuote' className='text-white underline underline-offset-4 hover:text-yellow-400'>Contact</a>
           </li>
         </ul>
       </nav>
