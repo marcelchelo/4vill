540e0fbe004a80dc82f53a87fe529f81a4f52df9
Marcelo Villalba <marcelo95326@icloud.com>
2024-11-01 15:43:25 -0400

    apos fix

--- app/getQuote/page.jsx
+++ app/getQuote/page.jsx
@@ -16,7 +16,7 @@ export default function Page()  {
     <div className='p-2 flex flex-col items-center justify-center mt-8'>
       <div className="text-center">
         <h1 className="font-mono font-black text-xl">We would love to partner with you on your next project.</h1>
-        <p className=' font-extralight mt-2 font-sans text-md sm:text-sm'> Send us your pdf's for an estimate.</p>
+        <p className=' font-extralight mt-2 font-sans text-md sm:text-sm'> Send us your pdf for an estimate.</p>
       </div>
       <QuoteForm/>
     </div>
