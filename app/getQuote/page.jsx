c631a2bc1b96170cbc3724ff3c15ff14b0b9c4d9
Marcelo Villalba <marcelo95326@icloud.com>
2024-11-01 15:41:50 -0400

    improved the form page

--- app/getQuote/page.jsx
+++ app/getQuote/page.jsx
@@ -12,20 +12,17 @@ export default function Page()  {
   return (
 
 <>
-
   <NavBarComponent/>
-    <div className='h-screen'>
-      
-      <div className="flex items-center justify-center flex-col">
-        <h1 className="font-bold font-mono text-2xl">Tell us about your project</h1>
-        <p className='font-light mt-2 font-sans text-lg'>We would love to partner with you on your next project. Contact us today.</p>
-
+    <div className='p-2 flex flex-col items-center justify-center mt-8'>
+      <div className="text-center">
+        <h1 className="font-mono font-black text-xl">We would love to partner with you on your next project.</h1>
+        <p className=' font-extralight mt-2 font-sans text-md sm:text-sm'> Send us your pdf's for an estimate.</p>
       </div>
-
-    
       <QuoteForm/>
-        
     </div>
+    <div>
+    </div>
+
 </>
    
   )
--- app/src/components/QuoteForm.jsx
+++ app/src/components/QuoteForm.jsx
@@ -12,6 +12,7 @@ export default function QuoteForm() {
     const [files, setFiles] = useState([]);
     const [zipcode, setZipcode] = useState('');
     const [superName, setSuperName] = useState('');
+    
     // send the files to a lambda function endpoint
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -23,76 +24,77 @@ export default function QuoteForm() {
     }
 
     return (
-    
-    <div className='flex justify-center items-center mt-6 p-2'>
+        <div className='flex justify-center items-center mt-4'>
+            <div className='p-2 rounded-md w-3/4 sm:w-full'>
+                <form className="sm:w-full " onSubmit={handleSubmit}>
+                    <div className="flex flex-wrap -mx-3 mb-6">
+                        <div className="w-full px-3">
+                            <label className="block uppercase tracking-wide text-gray-700 text-sm font-bold mb-2">
+                                Business or Home owner name:
+                                <input 
+                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+                                    type="text" 
+                                    value={companyName} 
+                                    onChange={(e) => setCompanyName(e.target.value)} 
+                                    placeholder="Your Name"
+                                />
+                            </label>
+                        </div>
+                    </div>
+                    <div className='flex flex-wrap -mx-3 mb-6'>
+                        <div className='w-full px-3'>
+                            <label className='block uppercase tracking-wide text-gray-700 text-sm font-bold mb-2'>
+                                Contact Phone:
+                                <input 
+                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+                                    type="text" 
+                                    placeholder="123-456-7890"
+                                    onChange={(e) => {
+                                        const phoneNumber = e.target.value;
+                                        if (phoneNumber.length === 10) {
+                                            setContactPhone(phoneNumber);
+                                        }
+                                    }}
+                                />
+                            </label>
+                        </div>
+                    </div>
+                    <div className="flex flex-wrap -mx-3 mb-6">
+                        <div className="w-full px-3">
+                            <label className="block uppercase tracking-wide text-gray-700 text-sm font-bold mb-2">
+                                Details:
+                                <textarea 
+                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 resize-y" 
+                                    value={details} 
+                                    onChange={(e) => setDetails(e.target.value)} 
+                                    placeholder="For an accurate quote please provide adequate information, and scope required."
+                                />
+                            </label>
+                        </div>
+                    </div>
+                    <div className="flex flex-wrap -mx-3 mb-6">
+                        <div className="w-full px-3">
+                            <label className="block uppercase tracking-wide text-gray-700 text-sm font-bold mb-2">
+                                Documents:
+                                <textarea 
+                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 resize-y" 
+                                    value={details} 
+                                    onChange={(e) => setDetails(e.target.value)} 
+                                    placeholder="Upload floor plan, in pdf format"
+                                />
+                            </label>
+                        </div>
+                    </div>
 
-        <div className='bg-slate-300 p-4 rounded-[8px] w-2/3'>
-        <form className="w-full max-w-lg">
-    <div className="flex flex-wrap -mx-3 mb-6">
-      <div className="w-full px-3">
-        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-          Business or Home owner name:
-          <input 
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
-            type="text" 
-            value={companyName} 
-            onChange={(e) => setCompanyName(e.target.value)} 
-            placeholder="Your Name"
-          />
-        </label>
-      </div>
-    </div>
-    <div className='flex flex-wrap -mx-3 mb-6'>
-      <div className='w-full px-3'>
-        <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>
-          Contact Phone:
-          <input 
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
-            type="text" 
-            placeholder="123-456-7890"
-            onChange={(e) => {
-              const phoneNumber = e.target.value;
-              if (phoneNumber.length === 10) {
-                setContactPhone(phoneNumber);
-              }
-            }}
-          />
-        </label>
-      </div>
-    </div>
-    {/* add input field for a text box called details  */}
-    <div className="flex flex-wrap -mx-3 mb-6">
-      <div className="w-full px-3">
-        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-          Details:
-          <textarea 
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 resize-y" 
-            value={details} 
-            onChange={(e) => setDetails(e.target.value)} 
-            placeholder="For an accurate quote please provide adequate information, and scope required."
-          />
-        </label>
-      </div>
-
-      {/* upload files */}
-      <div className="w-full px-3">
-        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-          Documents:
-          <textarea 
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 resize-y" 
-            value={details} 
-            onChange={(e) => setDetails(e.target.value)} 
-            placeholder="Upload floor plan, in pdf format"
-          />
-        </label>
-      </div>
-
-    </div>
-    <button className='bg-blue-400 p-2  rounded-md'>Submit</button>
-  </form>
-        <p className='text-sm'>We will contact you with an estimate for your project within 2 business days.</p>
-        <p className='text-sm'>A jobsite visit may be required.</p>
+                    <div className='flex justify-center'>
+                      <button className='bg-blue-400 p-2 rounded-md'>Submit</button>
+                    </div>
+                </form>
+                <div className='p-2 shadow-md mt-2'>
+                    <p className='text-xs font-extralight'>We will contact you with an estimate for your project within 2 business days.</p>
+                    <p className='text-xs'>A jobsite visit may be required.</p>
+                </div>
+            </div>
         </div>
-    </div>
     );
 }
